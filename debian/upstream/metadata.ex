9cde459ca0655339452fabb65b81f7addc7e0aa2
Steinar Bang <sb@dod.no>
2024-09-23 22:27:30 +0200

    Add real URLs in the upstream metadata file

--- debian/upstream/metadata.ex
+++ debian/upstream/metadata.ex
@@ -2,9 +2,9 @@
 # See https://wiki.debian.org/UpstreamMetadata for more info/fields.
 # Below an example based on a github project.
 
-# Bug-Database: https://github.com/<user>/libgourou/issues
-# Bug-Submit: https://github.com/<user>/libgourou/issues/new
+Bug-Database: https://forge.soutade.fr/soutade/libgourou/issues
+Bug-Submit: https://forge.soutade.fr/soutade/libgourou/issues/new
 # Changelog: https://github.com/<user>/libgourou/blob/master/CHANGES
 # Documentation: https://github.com/<user>/libgourou/wiki
-# Repository-Browse: https://github.com/<user>/libgourou
-# Repository: https://github.com/<user>/libgourou.git
+Repository-Browse: https://forge.soutade.fr/soutade/libgourou
+Repository: https://forge.soutade.fr/soutade/libgourou.git
